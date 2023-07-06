164172ed518fbe6d0503faf87c6706068fb560fd
belloblj2 <belloblj2@gmail.com>
2023-07-06 17:09:06 -0600

    Configure files

---
+++ redis.sh
@@ -0,0 +1,7 @@
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+
+yum module enable redis:remi-6.2 -y
+yum install redis -y
+cp redis.conf /etc/redis.conf
+systemctl enable redis
+systemctl start redis
