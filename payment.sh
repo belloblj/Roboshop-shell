4723cdcd236978442c4aa3c291527db0c106ee63
belloblj2 <belloblj2@gmail.com>
2023-07-10 12:45:23 -0600

    Correct file paths and Input Echo messages

--- payment.sh
+++ payment.sh
@@ -1,12 +1,26 @@
+echo -e "\e[36m>>>>>>>> Install Python 3.6 <<<<<<<<\e[0m"
 yum install python36 gcc python3-devel -y
+
+echo -e "\e[36m>>>>>>>> Add User <<<<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[36m>>>>>>>> Create App directory <<<<<<<<\e[0m"
 mkdir /app
+
+echo -e "\e[36m>>>>>>>> Download Application Code to Directory <<<<<<<<\e[0m"
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
+
+echo -e "\e[36m>>>>>>>> Unzip Payment App file <<<<<<<<\e[0m"
 cd /app
 unzip /tmp/payment.zip
-cd /app
+
+echo -e "\e[36m>>>>>>>> Download Dependencies <<<<<<<<\e[0m"
 pip3.6 install -r requirements.txt
-cp payment.service /etc/systemd/system/payment.service
+
+echo -e "\e[36m>>>>>>>> Copy Payment service files <<<<<<<<\e[0m"
+cp /root/Roboshop-shell/payment.service /etc/systemd/system/payment.service
+
+echo -e "\e[36m>>>>>>>> Load & Start Payment Service <<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable payment
 systemctl start payment
