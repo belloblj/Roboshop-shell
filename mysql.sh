65f77a02ee0845bcd108fba30ad4152ba8ec54e5
belloblj2 <belloblj2@gmail.com>
2023-07-10 12:25:10 -0600

    Correct file paths and Input Echo messages

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,15 @@
+echo -e "\e[36m>>>>>>>> CentOS-8 Comes with MySQL 8 Version by default,  our application needs MySQL 5.7. So lets disable MySQL 8 version <<<<<<<<\e[0m"
 yum module disable mysql -y
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "\e[36m>>>>>>>> Setup the MySQL5.7 repo file <<<<<<<<\e[0m"
+cp /root/Roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "\e[36m>>>>>>>> Install MySQL <<<<<<<<\e[0m"
 yum install mysql-community-server -y
+
+echo -e "\e[36m>>>>>>>> Start MySQL Service <<<<<<<<\e[0m"
 systemctl enable mysqld
-systemctl start mysqld
+systemctl restart mysqld
+
+echo -e "\e[36m>>>>>>>> Change default password to Roboshop1 <<<<<<<<\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
--- shipping.sh
+++ shipping.sh
@@ -1,16 +1,35 @@
+echo -e "\e[36m>>>>>>>> Install Maven <<<<<<<<\e[0m"
 yum install maven -y
+
+echo -e "\e[36m>>>>>>>> Add User <<<<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[36m>>>>>>>> Create Application Directory <<<<<<<<\e[0m"
+rm -rf /app
 mkdir /app
+
+echo -e "\e[36m>>>>>>>> Download Application code to Directory <<<<<<<<\e[0m"
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
+
+echo -e "\e[36m>>>>>>>> Unzip file <<<<<<<<\e[0m"
 cd /app
 unzip /tmp/shipping.zip
-cd /app
+
+echo -e "\e[36m>>>>>>>> Download Maven Dependencies and Build Application <<<<<<<<\e[0m"
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
-cp shipping.service /etc/systemd/system/shipping.service
+
+echo -e "\e[36m>>>>>>>> Copy and SetUp Shipping.service file into SystemD <<<<<<<<\e[0m"
+cp /root/Roboshop-shell/shipping.service /etc/systemd/system/shipping.service
+
+
+echo -e "\e[36m>>>>>>>> Install MySQL Client <<<<<<<<\e[0m"
+yum install mysql -y
+
+echo -e "\e[36m>>>>>>>> Load Schema <<<<<<<<\e[0m"
+mysql -h mysql.robobello.shop -uroot -pRoboShop@1 < /app/schema/shipping.sql
+
+echo -e "\e[36m>>>>>>>> Restart Shipping Service <<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable shipping
 systemctl start shipping
-yum install mysql -y
-mysql -h mysql.robobello.shop -uroot -pRoboShop@1 < /app/schema/shipping.sql
-systemctl restart shipping
